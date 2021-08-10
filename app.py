fc81d4fd4c13eafeee167be3d232bda36b68f077
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-08-10 09:31:20 -0400

    Precipitation and Station Analysis complete

---
+++ app.py
@@ -0,0 +1,41 @@
+# Import Dependencies
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Database Setup
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources\hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+app = Flask(__name__)
+
+
+# Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start_end<br/>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
